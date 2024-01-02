3241b5aa55a397fa44999762a9ad04caf2b97fc3
guntar <guntar@me.com>
2024-01-02 21:50:42 +0700

    71 Lifecycle hooks and home component

--- yamr2024/frontend/src/views/HomeView.vue
+++ yamr2024/frontend/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="home">
-    <h1>Homepage</h1>
-    <button class="btn btn-primary" @click="getQuestions">Get Questions</button>
-    <div v-if="this.questions">
+  <div class="home mt-3">
+    <!-- <h1>Homepage</h1> -->
+    <!-- <button class="btn btn-primary" @click="getQuestions">Get Questions</button> -->
+    <!-- <div v-if="this.questions">
       {{ this.questions }}
+    </div> -->
+    <div class="container">
+      <div v-for="question in questions" :key="question.pk">
+          <div class="card shadow p-3 mb-4 bg-body rounded">
+            <div class="card-body">
+              <p class="card-title">Posted by: <span class="question-author">{{ question.author }}</span></p>
+              <h5 class="card-text">{{ question.content }}</h5>
+              <p class="mb-2">Answers: {{ question.yamr_answers_count }}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div> 
+      </div>
     </div>
   </div>
 </template>
@@ -32,5 +44,26 @@ export default {
       }
     }
   },
+  created () {
+    console.log('HomeView created... lifecycle hook');
+    this.getQuestions();
+  }
 }
 </script>
+
+<style scoped>
+.question-author {
+  font-weight: bold;
+  color: #dc3545;
+}
+
+.question-link {
+  font-weight: 400;
+  color: black;
+  text-decoration: none;
+}
+
+.question-link:hover {
+  color: #343a40;
+}
+</style>
