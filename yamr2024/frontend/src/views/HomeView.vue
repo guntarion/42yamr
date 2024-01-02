88741db3cad843dff85495c333248d291b630a67
guntar <guntar@me.com>
2024-01-02 22:23:44 +0700

    73 Handle pagination in home component

--- yamr2024/frontend/src/views/HomeView.vue
+++ yamr2024/frontend/src/views/HomeView.vue
@@ -16,6 +16,17 @@
             </div>
           </div> 
       </div>
+       <div class="my-4">
+        <p v-show="loadingQuestions">...loading...</p>
+        <button
+          v-show="next"
+          @click="getQuestions"
+          class="btn btn-sm btn-outline-success"
+        >
+          Load More
+        </button>
+      </div>
+
     </div>
   </div>
 </template>
@@ -27,17 +38,31 @@ export default {
   name: 'HomeView',
   data () {
     return {
-      questions: []
+      questions: [],
+      next: null,
+      loadingQuestions: false
     }
   },
   methods: {
     async getQuestions () {
       let endpoint = '/api/v1/questions/';
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingQuestions = true;
       try {
         let response = await axios.get(endpoint);
-        console.log(response);
-        this.questions = response.data;
+        // console.log(response);
+        // this.questions = response.data; // Before adding pagination
+        // this.questions = response.data.results; // After adding pagination
+        this.questions.push(...response.data.results); // So that we can append the next page to the current page
+        this.loadingQuestions = false;
         console.log(this.questions);
+        if (response.data.next) {
+          this.next = response.data.next;
+        } else {
+          this.next = null;
+        }
       } catch (error) {
         console.log(error.response);
         alert(error.response.statusText);
