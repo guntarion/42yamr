0b24041a007cdf6880078678a8f2b5b6ddf00efd
guntar <guntar@me.com>
2024-01-05 22:33:38 +0700

    82. Like Answer Functionality

--- yamr2024/frontend/src/components/Answer.vue
+++ yamr2024/frontend/src/components/Answer.vue
@@ -15,10 +15,24 @@
                 Yes, delete my answer!
             </button>
         </div>
+         <div v-else>
+          <button type="button" class="btn"
+            @click="toggleLike"
+            :class="{
+                'btn-warning': userLikedAnswer,
+                'btn-outline-danger': !userLikedAnswer,
+            }">
+            Like Answer&nbsp;
+            <span class="badge bg-danger">{{ likesCounter }}</span>
+          </button>
+        </div>
+        <hr />   
+
     </div>
 </template>
 
 <script>
+import { axios } from "@/common/api.service.js";
 export default {
     name: "AnswerComponent",
     props: {
@@ -34,6 +48,8 @@ export default {
     data() {
         return {
             showDeleteConfirmationBtn: false,
+            userLikedAnswer: this.answer.user_has_liked_answer,
+            likesCounter: this.answer.likes_count,
         };
     },
     computed: {
@@ -49,6 +65,33 @@ export default {
             // emit an event to delete an answer instance
             this.$emit("delete-answer", this.answer);
         },
+        toggleLike() {
+            this.userLikedAnswer === false ? this.likeAnswer() : this.unLikeAnswer();
+        },
+        async likeAnswer() {
+            this.userLikedAnswer = true;
+            this.likesCounter += 1;
+            const endpoint = `/api/v1/answers/${this.answer.uuid}/like/`;
+            try {
+                await axios.post(endpoint);
+            } catch (error) {
+                console.log(error.response);
+                alert(error.response);
+            }
+        },
+        async unLikeAnswer() {
+            this.userLikedAnswer = false;
+            this.likesCounter -= 1;
+            const endpoint = `/api/v1/answers/${this.answer.uuid}/like/`;
+            try {
+                await axios.delete(endpoint);
+            } catch (error) {
+                console.log(error.response);
+                alert(error.response);
+            }
+        },
+        
     },
-}
+    
+};
 </script>
